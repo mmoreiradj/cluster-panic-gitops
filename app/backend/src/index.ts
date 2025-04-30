6450aec6beb33567171d88a3e2585ca27e57000a
Thomas Mauran <thomas.mauran@etu.umontpellier.fr>
2025-04-30 09:04:36 +0200

    feat: app

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -24,7 +24,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT),
+    port: parseInt(process.env.DB_PORT as string),
 });
 
 // Middleware
